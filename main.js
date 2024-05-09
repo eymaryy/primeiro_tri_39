bb1d1fed323807a16008316e4532abd8f1f0b7cb
eymaryy <104083328+eymaryy@users.noreply.github.com>
2024-05-08 21:20:32 -0300

    h

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao");
+
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+const botoes = document.querySelectorAll(".botao");
+
+const botoes = document.querySelectorAll(".botao");
+
+const botoes = document.querySelectorAll(".botao");
+
